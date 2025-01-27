d57097bcf55986cd77b70e875b16d446de277d91
DAVI ARAUJO SOCHA <davi.asocha@senacsp.edu.br>
2025-01-27 16:57:24 -0300

    primeira função PHP

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
 <?php 
-$cores = [
-    "vermelho","azul", "verde","amarelo","roxo"
-];
-//removendo o primeiro elemento 
-array_shift($cores);
-print_r($cores);
-//removendo o ultimo elemento
-array_pop($cores);
-print_r($cores);
-
+function teste(){
+    echo "Teste de função!!";
+}
+teste();
 ?>
