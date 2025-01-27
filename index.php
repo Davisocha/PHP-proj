c2f7dba9e45645123261d212ceba29b56e5ba49d
DAVI ARAUJO SOCHA <davi.asocha@senacsp.edu.br>
2025-01-27 16:55:59 -0300

    removendo elementos do array

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
 <?php 
-$numeros = [1,4,6,9,1,5,9,12];
-//ordem crecente
-sort($numeros);
-echo "Ordem crescente: \n";
-print_r($numeros);
-
-//ordem decrecente
-rsort($numeros);
-echo "Ordem decrecente:\n";
-print_r($numeros);
+$cores = [
+    "vermelho","azul", "verde","amarelo","roxo"
+];
+//removendo o primeiro elemento 
+array_shift($cores);
+print_r($cores);
+//removendo o ultimo elemento
+array_pop($cores);
+print_r($cores);
 
 ?>
