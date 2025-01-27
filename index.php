155852c30d1a7976dfbedabb0a6ec7745169ccd9
DAVI ARAUJO SOCHA <davi.asocha@senacsp.edu.br>
2025-01-27 17:12:09 -0300

    função de calcular raiz!

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
 <?php 
-function teste($n){
-    echo "Teste de função com parametro: $n";
+function calcularRaiz($numero){
+//verifica se o numero é não negativo
+if ($numero <0 ){
+    return "insira um numero não negativo";
+}    
+//calcula a raiz usando a função sqrt
+$raizQuadrada = sqrt($numero);
+//arredonda para duas casas decimais 
+$raizArredondada = round($raizQuadrada,2);
+return $raizArredondada;
 }
-teste(1);
-?>
+$numero = 10;
+$resul = calcularRaiz($numero);
+echo "A raiz quadrada de $numero é $resul";
+
+//resultado da função 
+/*A raiz quadrada de 10 é 3.16*/
+    ?>
