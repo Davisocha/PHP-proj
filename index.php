50ba4e6c9f9e18be79abcc6609b969ec17788410
DAVI ARAUJO SOCHA <davi.asocha@senacsp.edu.br>
2025-01-27 16:40:19 -0300

    exibição de arrays

--- index.php
+++ index.php
@@ -1,19 +1,16 @@
 <?php 
-//arrays multidimensionais
-$times = [
-    "time 1" => ["flamengo","curintia","palmeiras"],
-    "time 2" => ["Barcelona","Real madrid","Atletico de madrid"]
+$aluno= [
+    "nome" => "joão",
+    "idade"=> 20,
+    "curso" => "Sistemas de informação"
 ];
+//adicionando novo campo
+$aluno["semestre"]=4;
 
-//exibindo times brasileiros 
-echo "Times brasileiros: "."<br>";
-print_r($times["time 1"]);
+//exibindo valores
+echo "nome: ". $aluno["nome"]."<br>";
+echo "Curso: ". $aluno["curso"]."<br>";
 
-//adicionando um novo time
-$times["time 2"][]= "sevilla";
-print_r($times);
 
-//RESULTADO
-// Times brasileiros:
-// Array ( [0] => flamengo [1] => curintia [2] => palmeiras ) Array ( [time 1] => Array ( [0] => flamengo [1] => curintia [2] => palmeiras ) [time 2] => Array ( [0] => Barcelona [1] => Real madrid [2] => Atletico de madrid [3] => sevilla ) )
+
 ?>
