380ee5a6d11e6670d0604e7e56ab46b7b64c6d69
DAVI ARAUJO SOCHA <davi.asocha@senacsp.edu.br>
2025-01-29 16:47:26 -0300

    buscador de caracteres, que conta a posição dos caracteres pesquisados

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php 
-$nome ="Davi";
-echo strlen($nome)."<br>";
-$numcaractere = strlen($nome);
-echo $numcaractere;
+$texto = "Ti97 trabalhando com php";
+echo strpos($texto,"php");
+//ele procura a palavra em que coloquei na variavel e me da a posição dela por exemplo essa deu 21, por contar os caracteres
     ?>
     
