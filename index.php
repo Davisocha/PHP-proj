5e7f553991e14f288862038df97cd972e7cf90c5
DAVI ARAUJO SOCHA <davi.asocha@senacsp.edu.br>
2025-01-29 16:41:45 -0300

    contador de caracteres

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
 <?php 
-$str = "         p e s s o a l           ";
-echo trim($str)."<br>"; //saida exemplo
-
-$nome = "Wilton";
-//aspas duplas
-echo "Meu nome é: $nome"."<br>";
-echo 'Meu nome é:' .$nome."<br>";
-
+$nome ="Davi";
+echo strlen($nome)."<br>";
+$numcaractere = strlen($nome);
+echo $numcaractere;
     ?>
     
