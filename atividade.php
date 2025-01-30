1fc21f88049a9343d662abce930c0878eebe0164
Davisocha <daviaraujosocha@gmail.com>
2025-01-30 11:07:32 -0300

    base do css feito

--- atividade.php
+++ atividade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
@@ -9,35 +9,39 @@
 </head>
 
 <body>
-
     <main class="principal">
         <h1>ECoficiência</h1>
         <ul>
             <li class=" listas"><a href="cadastro.html">Cadastro</a></li>
         </ul>
     </main>
-    <section>
-        <div class="div-primeiro">
-            <h2>O que é ECoeficiência?</h2>
-            <p>Coeficiência é um conceito que combina eficiÊncia e cooperação para otimizar recursos e melhorar resultados em diversos contextos, como ambiental, empresarial e social</p>
-        </div>
-    </section>
-    <section>
-        <h2>Importância da Coeficiência</h2>
-        <p>Aplicar Coeficiência permite reduzir deperdícios, aumentar a produtividade e promover um desenvolvimento sustentável, impactando positivamente a economia e o meio ambiente.</p>
-    </section>
-    <section>
-        <h2>Beneficios</h2>
-        <ul>
-            <li>Redução do consumo de recursos naturais</li>
-            <li>Melhoria da sustentabilidade corporativa</li>
-            <li>Otimização de processos produtivos</li>
-            <li>Maior colaboração e inovação</li>
+    <div class="cards">
+        <section class="section-primeiro">
+            <div class="div-primeiro">
+                <h2>O que é ECoeficiência?</h2>
+                <p>Coeficiência é um conceito que combina eficiÊncia e cooperação para otimizar recursos e melhorar resultados em diversos contextos, como ambiental, empresarial e social</p>
+            </div>
+        </section>
+        <section>
+            <div class="div-primeiro">
+                <h2>Importância da Coeficiência</h2>
+                <p>Aplicar Coeficiência permite reduzir deperdícios, aumentar a produtividade e promover um desenvolvimento sustentável, impactando positivamente a economia e o meio ambiente.</p>
+            </div>
+        </section>
+        <section>
+            <div class="div-primeiro">
+                <h2>Beneficios</h2>
+                <ul>
+                    <li>Redução do consumo de recursos naturais</li>
+                    <li>Melhoria da sustentabilidade corporativa</li>
+                    <li>Otimização de processos produtivos</li>
+                    <li>Maior colaboração e inovação</li>
 
-        </ul>
-    </section>
-
-    <footer>
+                </ul>
+            </div>
+        </section>
+    </div>
+    <footer class="rodape">
         <p>© 2025 Coeficiência - Todos os direitos reservados</p>
     </footer>
 </body>
